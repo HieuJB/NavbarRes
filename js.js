9a32e7841d126343795cfe7a91d04f8f98677f3a
HieuJB <trunghieuit061099@gmail.com>
2021-01-02 16:53:15 +0700

    done-code

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-$("#btn1" ).click(function() {     
-    $('.menu').toggle();    
+    $("#btn1" ).click(function() {     
+        $('.menu').toggle();
+    });
 });
+$(window).resize(function() {
+    if( $(window).width() > 840 ) {
+        $('.menu').show();   
+    }
+    if( $(window).width() < 840 ) {
+        $('.menu').hide();   
+    }
 });
